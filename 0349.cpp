4714a71078dfb2404efb725b55cc7b66e8591f82
Alex Liu <liucongzhi@gmail.com>
2020-02-13 20:12:06 -0500

    Add solution for 0349

---
+++ 0349.cpp
@@ -0,0 +1,29 @@
+// 349. Intersection of Two Arrays
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int> intersec;
+        set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), back_inserter(intersec));
+        vector<int>::iterator end_unique = unique(intersec.begin(), intersec.end());
+        intersec.erase(end_unique, intersec.end());
+        return intersec;
+    }
+};
+
+int main(void) {
+    Solution s;
+    vector<int> nums1 = {4,9,5};
+    vector<int> nums2 = {9,4,9,8,4};
+    vector<int> intersec = s.intersection(nums1, nums2);
+    for (int i : intersec) cout << i << " ";
+    return 0;
+}
