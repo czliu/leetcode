b570ad6a8582cec36ba80040a7ce41cc9e1ac5dc
Alex Liu <liucongzhi@gmail.com>
2020-03-02 23:05:42 -0500

    Add 0018

---
+++ _0018/Solution.java
@@ -0,0 +1,50 @@
+// 0018. 4Sum
+// 双指针，剪枝，去重
+// 复习
+
+package _0018;
+
+import java.util.*;
+
+class Solution {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new LinkedList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+            if (nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1] < target) continue;
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j - i > 1 && nums[j] == nums[j - 1]) continue;
+                if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+                if (nums[i] + nums[j] + nums[n - 2] + nums[n - 1] < target) continue;
+
+                int left = j + 1;
+                int right = n - 1;
+                while (left < right) {
+                    int tmp = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (tmp == target) {
+                        List<Integer> tmpList = new LinkedList<>(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        ans.add(tmpList);
+                        while (left < right && nums[left] == nums[left + 1]) left += 1;
+                        while (left < right && nums[right] == nums[right - 1]) right -= 1;
+                        left += 1;
+                        right -= 1;
+                    } else if (tmp > target) right -= 1;
+                    else left += 1;
+                }
+            }
+
+        }
+        return ans; 
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1,0,-1,0,-2,2};
+        List<List<Integer>> output = s.fourSum(nums, 0);
+        System.out.println(output);
+    }
+}
