3ee13dd38f066a1a94b114669cc5410a098bb652
Alex Liu <liucongzhi@gmail.com>
2020-03-03 19:34:42 -0500

    Modify 0206

--- _0206/Solution.java
+++ _0206/Solution.java
@@ -23,9 +23,6 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        Solution s = new Solution();
-        int[] nums = {4,1,2,1,2};
-        int result1 = s.singleNumber(nums);
-        System.out.println(result1);
+        
     }
 }
