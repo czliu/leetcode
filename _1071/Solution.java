c65c123e6c1d33d6de0ad6f457891ef37ca4e716
Alex Liu <liucongzhi@gmail.com>
2020-03-15 18:50:20 -0400

    Add 1071

--- _1071/Solution.java
+++ _1071/Solution.java
@@ -1,41 +1,26 @@
-// 0039. Combination Sum
-// 回溯法
-// 复习
+// 1071. Greatest Common Divisor of Strings
+// 最大公约数
 
 
-package _0039;
+package _1071;
 
-import java.util.*;
+// import java.util.*;
 
 class Solution {
 
-    List<List<Integer>> ans =  new LinkedList<>();
-
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        LinkedList<Integer> track = new LinkedList<>();
-        Arrays.sort(candidates);
-        backtrack(candidates, 0, target, track);
-        return ans;
+    public String gcdOfStrings(String str1, String str2) {
+        // 假设str1是N个x，str2是M个x，那么str1+str2肯定是等于str2+str1的。
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        // 辗转相除法求gcd。
+        return str1.substring(0, gcd(str1.length(), str2.length()));
     }
 
-    private void backtrack(int[] candidates, int start, int target, LinkedList<Integer> track) {
-        if (target < 0) return;
-        if (target == 0){
-            ans.add(new LinkedList<>(track));
-            return;
-        }
-        for(int i = start;i < candidates.length;i++){
-            if(target < candidates[i]) break;
-            track.add(candidates[i]);
-            backtrack(candidates,i,target-candidates[i],track);
-            track.removeLast();
-        }
+    private int gcd(int a, int b) {
+        return b == 0? a: gcd(b, a % b);
     }
 
     public static void main(String[] args) {
-        Solution s = new Solution();
-        int[] nums = {2,3,5};
-        List<List<Integer>> output = s.combinationSum(nums, 8);
-        for (List<Integer> i:output) System.out.println(i);
     }
 }
