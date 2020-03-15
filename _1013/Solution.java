b555843349c225a17edf5a2ead62916755d7b0e3
Alex Liu <liucongzhi@gmail.com>
2020-03-15 18:50:13 -0400

    Add 1013

--- _1013/Solution.java
+++ _1013/Solution.java
@@ -1,41 +1,36 @@
-// 0039. Combination Sum
-// 回溯法
-// 复习
+// 1013. Partition Array into three parts with equal sum
+// 
 
 
-package _0039;
+package _1013;
 
-import java.util.*;
+// import java.util.*;
 
 class Solution {
 
-    List<List<Integer>> ans =  new LinkedList<>();
+    public boolean canThreePartsEqualSum(int[] A) {
+        int sum = 0;
+        for (int i:A) sum += i;
+        if (sum%3 != 0) return false;
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        LinkedList<Integer> track = new LinkedList<>();
-        Arrays.sort(candidates);
-        backtrack(candidates, 0, target, track);
-        return ans;
-    }
-
-    private void backtrack(int[] candidates, int start, int target, LinkedList<Integer> track) {
-        if (target < 0) return;
-        if (target == 0){
-            ans.add(new LinkedList<>(track));
-            return;
+        int left = 0;
+        int right = A.length-1;
+        int sumLeft = A[0];
+        int sumRight = A[A.length-1];
+        while (left + 1 < right) {
+            if (sumLeft == sum/3 && sumRight == sum/3) return true;
+            else if (sumLeft != sum/3) {
+                sumLeft += A[left+1];
+                left++; 
+            }
+            else if (sumRight != sum/3) {
+                sumRight += A[right-1];
+                right--; 
+            }
         }
-        for(int i = start;i < candidates.length;i++){
-            if(target < candidates[i]) break;
-            track.add(candidates[i]);
-            backtrack(candidates,i,target-candidates[i],track);
-            track.removeLast();
-        }
+        return false;
     }
 
     public static void main(String[] args) {
-        Solution s = new Solution();
-        int[] nums = {2,3,5};
-        List<List<Integer>> output = s.combinationSum(nums, 8);
-        for (List<Integer> i:output) System.out.println(i);
     }
 }
