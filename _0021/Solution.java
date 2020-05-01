5718a30611f4451a431ac79769e7759523ece75c
Alex Liu <liucongzhi@gmail.com>
2020-04-30 23:03:22 -0400

    add 0021

---
+++ _0021/Solution.java
@@ -0,0 +1,41 @@
+// 0021. Merge two sorted lists
+
+// #easy #linkedlist #review
+
+package _0021;
+
+import structure.ListNode;
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+                cur = cur.next;
+            }
+            else {
+                cur.next = l2;;
+                l2 = l2.next;
+                cur = cur.next;
+            }
+        }
+        if (l1 == null) cur.next = l2;
+        else cur.next = l1;
+        return head.next;
+    }
+}
