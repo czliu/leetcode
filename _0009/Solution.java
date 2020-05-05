f4aaa2937fc4b4122d8d7f2cd016a3a26af9a719
Alex Liu <liucongzhi@gmail.com>
2020-05-04 23:04:22 -0400

    add 0009

---
+++ _0009/Solution.java
@@ -0,0 +1,21 @@
+// 0009. palindrome number
+// -
+// #easy
+
+package _0009;
+
+public class Solution {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        String s = Integer.toString(x);
+        int i=0, j=s.length()-1;
+        while (i<j){
+            if (s.charAt(i) != s.charAt(j)) return false;
+            ++i;
+            --j;
+        }
+        return true;
+    }
+    
+}
