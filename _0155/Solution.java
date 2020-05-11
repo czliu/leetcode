5cea48debb2ecc455b3f75b86378818de02e1307
Alex Liu <liucongzhi@gmail.com>
2020-05-11 15:24:06 -0400

    add 0155

---
+++ _0155/Solution.java
@@ -0,0 +1,45 @@
+// 0155. Min stack
+// #easy #stack
+// *
+
+package _0155;
+
+import java.util.*;
+
+class MinStack {
+
+    Stack<Integer> xStack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        minStack.push(Integer.MAX_VALUE);
+    }
+    
+    public void push(int x) {
+        xStack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
+    }
+    
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return xStack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
