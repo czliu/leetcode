5d33f5198a9f1f14e573059dfce7e5b38d7c4836
Alex Liu <liucongzhi@gmail.com>
2020-05-12 11:13:00 -0400

    add 0507

---
+++ _0507/Solution.java
@@ -0,0 +1,24 @@
+// 0507. perfect number
+// #easy
+// *
+
+package _0507;
+
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) {
+                    sum += num / i;
+                }
+
+            }
+        }
+        return sum - num == num;
+    }
+}
