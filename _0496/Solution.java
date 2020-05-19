94b9c10bc94e55a86ad31f72442502f819d09047
Alex Liu <liucongzhi@gmail.com>
2020-05-19 18:26:37 -0400

    0496

---
+++ _0496/Solution.java
@@ -0,0 +1,26 @@
+// 0496. next greater element I
+// #easy #单调栈 #monotoneStack
+// *
+
+package _0496;
+
+import java.util.*;
+
+public class Solution {
+    public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        Stack < Integer > stack = new Stack < > ();
+        HashMap < Integer, Integer > map = new HashMap < > ();
+        int[] res = new int[findNums.length];
+        for (int i = 0; i < nums.length; i++) {
+            while (!stack.empty() && nums[i] > stack.peek())
+                map.put(stack.pop(), nums[i]);
+            stack.push(nums[i]);
+        }
+        while (!stack.empty())
+            map.put(stack.pop(), -1);
+        for (int i = 0; i < findNums.length; i++) {
+            res[i] = map.get(findNums[i]);
+        }
+        return res;
+    }
+}
