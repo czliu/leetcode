23a7c75e40ad9392b046e84c446afc3d904458d5
Alex Liu <liucongzhi@gmail.com>
2020-05-20 11:36:48 -0400

    add 0521

---
+++ _0521/Solution.java
@@ -0,0 +1,15 @@
+// 0521. Longest Uncommon Subsequence I 
+// #easy
+// -
+
+package _0521;
+
+public class Solution {
+
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b))
+            return -1;
+        return Math.max(a.length(), b.length());
+    }
+
+}
