7a1b4915b2eb72a6f39f118294cc0b7daf7544ae
Alex Liu <liucongzhi@gmail.com>
2020-05-23 08:48:30 -0400

    0653

---
+++ _0653/Solution.java
@@ -0,0 +1,44 @@
+// 0653. 两数之和 IV - 输入 BST
+// #easy
+// *
+
+package _0653;
+
+import structure.TreeNode;
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    // 可以用hashset, 也可以用list, 这里用list
+    public boolean findTarget(TreeNode root, int k) {
+        List < Integer > list = new ArrayList<>();
+        inorder(root, list);
+        int l = 0, r = list.size() - 1;
+        while (l < r) {
+            int sum = list.get(l) + list.get(r);
+            if (sum == k)
+                return true;
+            if (sum < k)
+                l++;
+            else
+                r--;
+        }
+        return false;
+    }
+    public void inorder(TreeNode root, List < Integer > list) {
+        if (root == null)
+            return;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+}
