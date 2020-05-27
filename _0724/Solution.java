5d1b888d43ddc524455da90897c6a0305aac7d39
Alex Liu <liucongzhi@gmail.com>
2020-05-26 21:40:15 -0400

    0724

---
+++ _0724/Solution.java
@@ -0,0 +1,17 @@
+// 0724.寻找数组的中心索引
+// #easy
+// -
+
+package _0724;
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum = 0, leftsum = 0;
+        for (int x: nums) sum += x;
+        for (int i = 0; i < nums.length; ++i) {
+            if (leftsum == sum - leftsum - nums[i]) return i;
+            leftsum += nums[i];
+        }
+        return -1;
+    }
+}
