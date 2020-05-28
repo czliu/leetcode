cb88ef762933000148927de78f9d254d6a774abc
Alex Liu <liucongzhi@gmail.com>
2020-05-28 12:49:32 -0400

    762

---
+++ _0762/Solution.java
@@ -0,0 +1,19 @@
+// 762. 二进制表示中质数个计算置位
+// #easy
+// -
+
+package _0762;
+
+class Solution {
+    public int countPrimeSetBits(int L, int R) {
+        int ans = 0;
+        for (int x = L; x <= R; ++x)
+            if (isSmallPrime(Integer.bitCount(x)))
+                ++ans;
+        return ans;
+    }
+    public boolean isSmallPrime(int x) {
+        return (x == 2 || x == 3 || x == 5 || x == 7 ||
+                x == 11 || x == 13 || x == 17 || x == 19);
+    }
+}
