5ac2ae6ab3374472365f6b5bf7f7ee9769f7b391
Alex Liu <liucongzhi@gmail.com>
2020-05-27 21:27:10 -0400

    0746

---
+++ _0746/Solution.java
@@ -0,0 +1,21 @@
+// 746. 使用最小花费爬楼梯
+// #easy #dp
+// -
+
+package _0746;
+
+class Solution {
+
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        if (n <= 1) return 0;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i=2;i<n+1;++i) {
+            dp[i] = Math.min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]);
+        }
+        return dp[n];
+    }
+
+}
