344a8e086f1b5dda157d142a7efb30a9b7ffe0d6
Alex Liu <liucongzhi@gmail.com>
2020-06-07 11:17:12 -0400

    0908

---
+++ _0908/Solution.java
@@ -0,0 +1,26 @@
+// 908. 最小差值 I
+// #easy
+// -
+
+package _0908;
+
+import java.sql.SQLOutput;
+
+class Solution {
+    public int smallestRangeI(int[] A, int K) {
+        int min = A[0], max = A[0];
+        int i = 0;
+        while (i < A.length) {
+            if (A[i] < min) min = A[i];
+            else if (A[i] > max) max = A[i];
+            ++i;
+        }
+        int ans = (max - K) - (min + K);
+        return Math.max(ans, 0);
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int ans = s.smallestRangeI(new int[] {0,10}, 2);
+    }
+}
