5a631aaf6b20ab10192427898e4c45b5ee923337
Alex Liu <liucongzhi@gmail.com>
2020-06-08 23:05:11 -0400

    0965

---
+++ _0965/Solution.java
@@ -0,0 +1,26 @@
+// 965. 单值二叉树
+// #easy
+// *
+
+package _0965;
+
+import structure.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        boolean left_correct = (root.left == null ||
+                (root.val == root.left.val && isUnivalTree(root.left)));
+        boolean right_correct = (root.right == null ||
+                (root.val == root.right.val && isUnivalTree(root.right)));
+        return left_correct && right_correct;
+    }
+}
