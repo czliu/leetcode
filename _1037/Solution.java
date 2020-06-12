50573ac7f488dbd05d5904855d83a352aefd37bf
Alex Liu <liucongzhi@gmail.com>
2020-06-12 14:09:47 -0400

    1037

---
+++ _1037/Solution.java
@@ -0,0 +1,11 @@
+// 1037. 有效的回旋镖
+// #easy
+// *
+
+package _1037;
+
+class Solution {
+    public boolean isBoomerang(int[][] points) {
+        return (points[1][1] - points[0][1]) * (points[2][0] - points[0][0]) != (points[2][1] - points[0][1]) * (points[1][0] - points[0][0]);
+    }
+}
