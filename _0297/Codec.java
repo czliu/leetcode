cc82e8d6b32eec51cade5e8d2bac06f9fa3c1c04
Alex Liu <liucongzhi@gmail.com>
2020-06-15 13:43:57 -0400

    0297

---
+++ _0297/Codec.java
@@ -0,0 +1,62 @@
+// 297. 二叉树的序列化与反序列化
+// #hard
+// *
+
+package _0297;
+
+import structure.TreeNode;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    public String serialize(TreeNode root) {
+        StringBuilder sb = ser_help(root, new StringBuilder());
+        return sb.toString();
+    }
+
+    public StringBuilder ser_help(TreeNode root, StringBuilder str){
+        if(null == root){
+            str.append("null,");
+            return str;
+        }
+        str.append(root.val);
+        str.append(",");
+        str = ser_help(root.left, str);
+        str = ser_help(root.right, str);
+        return str;
+    }
+
+    public TreeNode deserialize(String data) {
+        String[] str_word = data.split(",");
+        List<String> list_word = new LinkedList<String>(Arrays.asList(str_word));
+        return deser_help(list_word);
+    }
+
+    public TreeNode deser_help(List<String> li){
+        if(li.get(0).equals("null")){
+            li.remove(0);
+            return null;
+        }
+        TreeNode res = new TreeNode(Integer.valueOf(li.get(0)));
+        li.remove(0);
+        res.left = deser_help(li);
+        res.right = deser_help(li);
+        return res;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
