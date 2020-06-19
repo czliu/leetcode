7fb62f2f1e1481f55d9af389d55b8aaf352f6947
Alex Liu <liucongzhi@gmail.com>
2020-06-19 18:24:02 -0400

    1389

---
+++ _1389/Solution.java
@@ -0,0 +1,22 @@
+// 1389. 按既定顺序创建目标数组
+// #easy
+// -
+
+package _1389;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> list = new ArrayList<>();
+        for (int i=0;i<nums.length;++i) {
+            list.add(index[i], nums[i]);
+        }
+        int[] ans = new int[list.size()];
+        int i = 0;
+        for (int k : list) ans[i++] = k;
+        return ans;
+    }
+}
