de0575278e2325c9f2a57d37c61608f3381c20e5
Alex Liu <liucongzhi@gmail.com>
2020-06-22 13:28:04 -0400

    1480

---
+++ _1480/Solution.java
@@ -0,0 +1,14 @@
+// 1480. 一维数组的动态和
+// #easy
+// -
+
+package _1480;
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for (int i=1;i<nums.length;++i) {
+            nums[i] += nums[i-1];
+        }
+        return nums;
+    }
+}
