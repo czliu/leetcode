2038d664d5a94d0fdfeea857f06784bf6271fd17
Alex Liu <liucongzhi@gmail.com>
2020-07-03 10:36:13 -0400

    0073

---
+++ _0073/Solution.java
@@ -0,0 +1,94 @@
+// 73. 矩阵置零
+// #medium
+// **
+
+package _0073;
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        Boolean isCol = false;
+        int R = matrix.length;
+        int C = matrix[0].length;
+
+        for (int i = 0; i < R; i++) {
+
+            // Since first cell for both first row and first column is the same i.e. matrix[0][0]
+            // We can use an additional variable for either the first row/column.
+            // For this solution we are using an additional variable for the first column
+            // and using matrix[0][0] for the first row.
+            if (matrix[i][0] == 0) {
+                isCol = true;
+            }
+
+            for (int j = 1; j < C; j++) {
+                // If an element is zero, we set the first element of the corresponding row and column to 0
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        // Iterate over the array once again and using the first row and first column, update the elements.
+        for (int i = 1; i < R; i++) {
+            for (int j = 1; j < C; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // See if the first row needs to be set to zero as well
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < C; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // See if the first column needs to be set to zero as well
+        if (isCol) {
+            for (int i = 0; i < R; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
+
+class Solution2 {
+    public void setZeroes(int[][] matrix) {
+        // 如果题目规定所有数都是正数，MODIFIED可以为任意负数
+        // 如果没有这样的规定，那么可以遍历一遍matrix，MODIFIED选为没出现过的最小正数
+        int MODIFIED = -1000000;
+        int R = matrix.length;
+        int C = matrix[0].length;
+
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                if (matrix[r][c] == 0) {
+                    // We modify the corresponding rows and column elements in place.
+                    // Note, we only change the non zeroes to MODIFIED
+                    for (int k = 0; k < C; k++) {
+                        if (matrix[r][k] != 0) {
+                            matrix[r][k] = MODIFIED;
+                        }
+                    }
+                    for (int k = 0; k < R; k++) {
+                        if (matrix[k][c] != 0) {
+                            matrix[k][c] = MODIFIED;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                // Make a second pass and change all MODIFIED elements to 0 """
+                if (matrix[r][c] == MODIFIED) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+    }
+}
+
