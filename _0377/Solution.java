aca8011b022d9d4ff2573f696fa14ac02e23f541
Congzhi Liu <cliu166@ford.com>
2022-08-05 12:29:37 -0400

    0805

--- _0377/Solution.java
+++ _0377/Solution.java
@@ -36,3 +36,24 @@ package _0377;
 //         return ans;
 //     }
 // }
+
+public class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] tmp = new int[target+1];
+        for (int i=0;i<target+1;++i) tmp[i] = -1;
+        return recurse(nums, target, tmp);
+    }
+
+    public int recurse(int[] nums, int remain, int[] tmp) {
+        int len = nums.length;
+        if (remain < 0) return 0;
+        if (tmp[remain] != -1) return tmp[remain];
+        if (remain == 0) return 1;
+        int ans = 0;
+        for (int i=0;i<len;++i) {
+            ans += recurse(nums, remain-nums[i], tmp);
+        }
+        tmp[remain] = ans;
+        return ans;
+    }
+}
