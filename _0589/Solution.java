2b16d8861b8cfd72c34dfd9426c623aab687e884
Alex Liu <liucongzhi@gmail.com>
2022-08-08 18:06:42 -0400

    0808

--- _0589/Solution.java
+++ _0589/Solution.java
@@ -1,6 +1,25 @@
 // 0589. N-ary Tree Preorder Traversal
 // N-ary Tree, iterative 迭代
 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
 package _0589;
 
 import java.util.*;
